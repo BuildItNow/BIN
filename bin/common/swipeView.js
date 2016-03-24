7e2957194e57dd097a169a965e7d0f355d726816
Yang G <pinner@mail.ustc.edu.cn>
2016-03-24 11:52:52 +0800

    优化SwipeView，添加更多slide接口

--- bin/common/swipeView.js
+++ bin/common/swipeView.js
@@ -5,107 +5,149 @@ define(["bin/core/view", "swiper"], function(Base, Swiper)
 
 	};
 
+	var SWIPER_DEFAULT_OPTIONS = 
+	{
+		loop : false,
+		shortSwipes:true
+	};
+
 	Class.constructor = function(options)
 	{
-		this._initCurrent = options.current;
-		this._onChange    = options.onChange;
-		this._current     = null;
+		this._onChange      = options.onChange;
+		this._swiperOptions = options.swiperOptions || {}; 
+		if(options.current !== undefined)
+		{
+			this._swiperOptions.initialSlide = options.current;
+		}
+		else if(this._swiperOptions.initialSlide === undefined)
+		{
+			this._swiperOptions.initialSlide = 0;
+		}
+
+		_.defaults(this._swiperOptions, SWIPER_DEFAULT_OPTIONS);
+		var self = this;
+		this._swiperOptions.onSlideChangeEnd = function(swiper){self._onSwiperSwipe(swiper.activeIndex)};
 
 		Base.prototype.constructor.call(this, options);
 	}
 
 	Class.genHTML = function()
 	{
-		this._slides = [];
-		
 		this.$().addClass("swiper-container");
 		var children = this.$().children();
-		var child = null;
 
 		// Generate wrapper and slide holder
 		var wrapperHtml = '<div class="swiper-wrapper">';
 		for(var i=0,i_sz=children.length; i<i_sz; ++i)
 		{
-			child = $(children[i]);
-			child.detach();
-
-			this._slides.push(child);
+			this.$()[0].removeChild(children[i]);
 
 			wrapperHtml += '<div class="swiper-slide" style="background-color:transparent"></div>';
 		}
 		wrapperHtml += '</div>'
 		
-		this._slideWrapper = $(wrapperHtml);
+		this.$swiperWrapper = $(wrapperHtml);
+		var slideHolders = this.$swiperWrapper.children();
+		for(var i=0,i_sz=children.length; i<i_sz; ++i)
+		{
+			slideHolders[i].appendChild(children[i]);
+		}
 		
-		this.$().append(this._slideWrapper);
+		this.$().append(this.$swiperWrapper);
+	}
+
+	Class.appendSlide = function(slide, refresh)
+	{
+		var elem = $('<div class="swiper-slide" style="background-color:transparent"></div>');
+		elem.append(slide);
+
+		if(refresh)
+		{
+			this.swiper.appendSlide(elem[0]);
+		}
+		else
+		{
+			this.$swiperWrapper.append(elem);
+		}
+	}
+
+	Class.removeSlide = function(index, refresh)
+	{
+		if(refresh)
+		{
+			this.swiper.removeSlide(index);
+		}
+		else
+		{
+			$(this.$swiperWrapper.children()[index]).remove();
+		}
+	}
+
+	Class.removeAllSlides = function(refresh)
+	{
+		if(refresh)
+		{
+			this.swiper.removeAllSlides();
+		}
+		else
+		{
+			this.$swiperWrapper.html("");
+		}
+	}
+
+	Class.posGenHTML = function()
+	{
+		this.swiper = new Swiper(this.$(), this._swiperOptions);
+		if(this._swiperOptions.initialSlide === 0)	// Swiper don't trigger event when initialSlide is 0
+		{
+			this._onSwiperSwipe(this._swiperOptions.initialSlide);
+		}
 	}
 
 	Class.asyncPosGenHTML = function()
 	{
-		// Add content slide to holder
-		var slideHolders = this._slideWrapper.children();
-		for(var i=0,i_sz=this._slides.length; i<i_sz; ++i)
-		{
-			$(slideHolders[i]).append(this._slides[i]);
-		}
-
-		delete this._slides;
-		delete this._slideWrapper;
-
-		var self    = this;
-		var options = 
-		{
-			onSlideChangeEnd : function(swiper){self._onSwiperSwipe(swiper.activeIndex)},
-			initialSlide : this._initCurrent ? this._initCurrent : 0,
-			loop : false,
-			shortSwipes:true,
-		};
-
-		delete this._initCurrent;
-
-		this._swiper = new Swiper(this.$(), options);
-		if(options.initialSlide === 0)	// Swiper don't trigger event when initialSlide is 0
-		{
-			this._onSwiperSwipe(options.initialSlide);
-		}
+		this.refresh();
 	}
 
 	Class.setCurrent = function(index, noTrigger)
 	{
-		if(this.getCurrent() === index)
-		{
-			return ;
-		}
-
-		this._swiper.slideTo(index, undefined, !noTrigger);
-		if(noTrigger)
-		{
-			this._current = index;
-		}
+		this.swiper.slideTo(index, undefined, !noTrigger);
 	}
 
 	Class.getCurrent = function()
 	{
 
-		return this._current!==null ? this._current : this._initCurrent; 
+		return this.swiper.activeIndex; 
+	}
+
+	Class.slideNext = function(noTrigger)
+	{
+		this.swiper.slideNext(!noTrigger);
+	}
+
+	Class.slidePrev = function(noTrigger)
+	{
+		this.swiper.slidePrev(!noTrigger);
+	}
+
+	Class.refresh = function()
+	{
+		this.swiper.update();
 	}
 
 	Class.onRemove = function()
 	{
-		if(this._swiper)
+		if(this.swiper)
 		{
-			this._swiper.destroy(false);
-			delete this._swiper;
+			this.swiper.destroy(false);
 		}
 	}
 
 	Class._onSwiperSwipe = function(index)
 	{
-		this._current = index;
-
 		if(this._onChange)
 		{
-			this._onChange(this, this._current);
+			this._onChange(this, index);
 		}
 	}
 
